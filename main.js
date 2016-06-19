1eb490ce9eba521be4d9f95d04d83228c5d7f00e
Niclas Willems <niclas.willems@gmail.com>
2016-06-18 23:16:15 -0400

    Question 2 answered in 2 ways. Method 1 using filter and map. Method 2 using forEach.

--- main.js
+++ main.js
@@ -23,12 +23,50 @@ var answer1 = items.map(function (object) {
   return a + b;
 }) / items.length;
 
-document.getElementById("answer1").innerHTML = "The average prices is $" + Math.round(answer1 * 100) / 100;
+document.getElementById("answer1").innerHTML = "The average price is $" + Math.round(answer1 * 100) / 100;
 // The *100 / 100 allows you to Math.round the number and retain the first 2 decimal points.
 // Math.round on it's own will only return a whole number.
-
 
 //**********************************************************************//
 // Question 2 - Show me how to get an array of items that
 // cost between $14.00 and $18.00 USD
 //**********************************************************************//
+
+// Empty array to hold final values
+
+var finalArray = [];
+
+//*****************************
+//Method 1 using filter and map
+//*****************************
+
+//Use filter to pull all of the objects between $14-18
+
+// var finalArray = items.filter(function (item) {
+//   return (item.price > 14 && item.price < 18);
+// });
+
+//Use map to pull only the prices from the filtered items.
+
+// var titles = finalArray.map(function (item) {
+//   return item.title;
+// });
+
+//*****************************
+//Method 2 using forEach
+//*****************************
+
+// Loop through the array and selectively add the title to the price catcher array.
+items.forEach(function (item) {
+  if (item.price > 14 && item.price < 18) {
+    finalArray.push(item.title);
+    finalArray.push("<br />");
+  };
+});
+
+document.getElementById("answer2").innerHTML = "Items that cost between $14.00 USD and $18.00 USD:<p>" + finalArray;
+
+//***************************************************//
+// Question 3: Which item has a "GBP" currency code?
+// Display it's name and price.
+//***************************************************//
